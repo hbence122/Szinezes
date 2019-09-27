79abd53547eccdcf00bc8cce63738fc3969c0b6c
Horvath Bence <hbence122@gmail.com>
2019-09-27 19:05:42 +0200

    Feladatkesz(nem vegleges)

--- app/src/main/java/com/example/szinezes/MainActivity.java
+++ app/src/main/java/com/example/szinezes/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -16,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     private RelativeLayout relativeLayout;
     private Button button_RGB;
     private TextView textView_RGB;
+    private TextView textR;
+    private TextView textG;
+    private TextView textB;
+    private TextView valueR;
+    private TextView valueG;
+    private TextView valueB;
+    private SeekBar seek_R;
+    private SeekBar seek_G;
+    private SeekBar seek_B;
+
     int r,g,b = 0;
 
 
@@ -26,26 +37,120 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        szinTarolas();
 
-        button_RGB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Random rand=new Random();
-                r=rand.nextInt(256);
-                g=rand.nextInt(256);
-                b=rand.nextInt(256);
-                textView_RGB.setText("("+r+ ","+g+","+b+")");
-                relativeLayout.setBackgroundColor(Color.rgb(r,g,b));
-            }
-        });
+
+        seek_R.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean n) {
+
+                        r=i;
+                        szinTarolas();
+                        valueR.setText(String.valueOf(r));
+                        textView_RGB.setText("("+r+ ","+g+","+b+")");
+                        relativeLayout.setBackgroundColor(Color.rgb(r,g,b));
+
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        seek_G.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean n) {
+                        g=i;
+
+                        szinTarolas();
+
+                        valueG.setText(String.valueOf(g));
+
+                        textView_RGB.setText("("+r+ ","+g+","+b+")");
+                        relativeLayout.setBackgroundColor(Color.rgb(r,g,b));
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        seek_B.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean n) {
+                        b=i;
+                        szinTarolas();
+
+                        valueB.setText(String.valueOf(b));
+
+                        textView_RGB.setText("("+r+ ","+g+","+b+")");
+                        relativeLayout.setBackgroundColor(Color.rgb(r,g,b));
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+
+
+
     }
 
     public void init() {
 
 
         relativeLayout = findViewById(R.id.relativeLayout);
-        button_RGB=findViewById(R.id.button_RGB);
+
         textView_RGB=findViewById(R.id.textView_RGB);
+        textR=findViewById(R.id.textR);
+        textG=findViewById(R.id.textG);
+        textB=findViewById(R.id.textB);
+        valueR=findViewById(R.id.valueR);
+        valueG=findViewById(R.id.valueG);
+        valueB=findViewById(R.id.valueB);
+        seek_R=findViewById(R.id.seek_R);
+        seek_G=findViewById(R.id.seek_G);
+        seek_B=findViewById(R.id.seek_B);
+        relativeLayout.setBackgroundColor(Color.rgb(r,g,b));
+        textView_RGB.setText("("+r+ ","+g+","+b+")");
+        valueR.setText(String.valueOf(r));
+        valueG.setText(String.valueOf(g));
+        valueB.setText(String.valueOf(b));
+
+
+    }
+
+    private void szinTarolas() {
+        int color = Color.rgb(r, g, b);
 
     }
 }
